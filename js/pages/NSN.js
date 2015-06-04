d24169818067d7366940c9a35a8b9a68d91d030d
goldsun27 <todayhonest27@gmail.com>
2015-06-04 16:10:25 -0500

    New separated routing working as planned

--- js/pages/NSN.js
+++ js/pages/NSN.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { DefaultRoute, Link, Route, RouteHandler } from 'react-router';
 
 
 let NSNList = React.createClass({ 
@@ -11,10 +11,10 @@ let NSNList = React.createClass({
   		//let Nsn = router.getCurrentParams().nsn;
     	return(<div>
     		<ul>
-        <li><Link to="ndnDetail/1234-12-123-12345">1234-12-123-12345</Link></li>
-        <li><Link to="ndnDetail/1234-12-123-12346">1234-12-123-12346</Link></li>
-        <li><Link to="ndnDetail/1234-12-123-12347">1234-12-123-12347</Link></li>
-        <li><Link to="ndnDetail/1234-12-123-12348">1234-12-123-12348</Link></li>
+        <li><Link to="nsnDetail" params={{nsn: "1234-12-123-12345"}}>1234-12-123-12345</Link></li>
+        <li><Link to="nsnDetail" params={{nsn: "1234-12-123-12346"}}>1234-12-123-12346</Link></li>
+        <li><Link to="nsnDetail" params={{nsn: "1234-12-123-12347"}}>1234-12-123-12347</Link></li>
+        <li><Link to="nsnDetail" params={{nsn: "1234-12-123-12348"}}>1234-12-123-12348</Link></li>
         </ul>
 			</div>
     	);
--- js/routes/appRoutes.js
+++ js/routes/appRoutes.js
@@ -1,17 +1,17 @@
 
 import React from 'react';
 import Router from 'react-router';
+import { DefaultRoute, Link, Route, RouteHandler } from 'react-router';
 
-
-let LoginHandler =  require('../pages/Login');
-let ContactHandler =  require('../pages/ContactUs');
-let NsnDetailHandler =  require('../pages/NSNDetail');
-let NsnListHandler =  require('../pages/NSN');
+import LoginHandler from '../pages/Login';
+import ContactHandler from '../pages/ContactUs';
+import NsnDetailHandler from '../pages/NSNDetail';
+import NsnListHandler from '../pages/NSN';
 
 
 
 let Routes = (  
-  <Route name="app" path="/" handler={App}>
+  <Route name="app" path="/">
     <Route name="login" path="/login" handler={LoginHandler}/>,
     <Route name="contactus" path="/contactus" handler={ContactHandler}/>,
     <Route name="nsn" path="/NSN" handler={NsnListHandler}/>,
